a31bdc8cec77efef23d9b3c4a3c46b2a5d81f0c8
紫竹 <luozhu2021@gmail.com>
2023-07-16 15:02:07 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# @openfly/template-demo
+
+> openfly template demo
+
+## Usage
+
+```sh
+$ npx @openfly/cli init
+
+$ ? Pick template source › - Use arrow-keys. Return to submit.
+$ ❯   Git Repository
+$     NPM Package
+
+$ ? Input template name › zizhuspot/fly--blog
+```
---
+++ fly.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  prompts: [
+    {
+      type: 'text',
+      name: 'title',
+      message: 'Input site title'
+    },
+    {
+      type: 'text',
+      name: 'subtitle',
+      message: 'Input site subtitle'
+    },
+    {
+      type: 'text',
+      name: 'description',
+      message: 'Input site description'
+    },
+    {
+      type: 'text',
+      name: 'author',
+      message: 'Input author name'
+    },
+    {
+      type: 'text',
+      name: 'url',
+      message: 'Input site url'
+    },
+    {
+      type: 'text',
+      name: 'logo',
+      message: 'Input site logo'
+    }
+  ]
+}
